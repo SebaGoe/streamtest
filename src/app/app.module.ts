8dc0be0933ab1b6d65477fb15db66d57965d6d1d
Sebastian Goerick <behindjapan@gmail.com>
2020-04-27 11:17:01 +0900

    Use local HLS stream and display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { environment } from '../environments/environment';
 import { VideoPlayerComponent } from './video-player/video-player.component';
 import { Camera360Component } from './camera360/camera360.component';
 import { HomeComponent } from './home/home.component';
+import { MotionSensorComponent } from './motion-sensor/motion-sensor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideoPlayerComponent,
     Camera360Component,
-    HomeComponent
+    HomeComponent,
+    MotionSensorComponent
   ],
   imports: [
     BrowserModule,
--- src/app/camera360/camera360.component.ts
+++ src/app/camera360/camera360.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 declare  var Kaleidoscope:  any;
+declare var Hls: any;
 
 @Component({
   selector: 'app-camera360',
@@ -24,16 +25,57 @@ export class Camera360Component implements OnInit {
     // }, 3000)
   }
   startAll() {
-      this.start1();
+      this.start0();
+  //    this.start1();
       this.start2();
       this.start3();
       this.start4();
   }
 
+  start0() {
+    var containerSelector = '#container360-1';
+    if (Hls.isSupported()) {
+        var hls = new Hls();
+        var video = document.createElement('video');
+
+        hls.loadSource('http://localhost:8000/test.m3u8');
+      //  hls.loadSource('https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8');
+        hls.attachMedia(video);
+
+        hls.on(Hls.Events.MANIFEST_PARSED,function() {
+            video.play()
+      });
+
+      /**
+       * 
+ffmpeg -i cool.mp4 -profile:v baseline -level 3.0 -s 640x360 -start_number 0 -hls_time 10 -hls_list_size 0 -f hls index.m3u8
+ffmpeg -re -i rollercoaster.mp4 -c:v libx264 -preset superfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost/live/rollercoaster
+
+       */
+
+      this.viewer1 = new Kaleidoscope.Video({
+          source: video,
+          containerId: containerSelector,
+          height: window.innerHeight,
+          width: window.innerWidth
+      });
+
+      this.viewer1.render();
+      window.onresize = this.viewer1.setSize({height: window.innerHeight, width: window.innerWidth});
+ 
+      document.querySelector(containerSelector).addEventListener('touchend', this.viewer1.play.bind(this.viewer1));
+      
+      document.querySelector(containerSelector)
+      .addEventListener('touchend', () => this.viewer1.play(this.viewer1));
+    } else {
+        alert("HLS.js doesn't work with your current browser");
+    }
+  }
+
   start1() {
     var containerSelector = '#container360-1';
      this.viewer1 = new Kaleidoscope.Video({
-         source: '../../assets/videos/cool.mp4',// './assets/cool.mp4',
+         source: '../../assets/videos/equi.mp4',// './assets/cool.mp4',
          containerId: containerSelector,
          height: window.innerHeight,
          width: window.innerWidth
@@ -54,7 +96,7 @@ export class Camera360Component implements OnInit {
   start2() {
     var containerSelector = '#container360-2';
      this.viewer2 = new Kaleidoscope.Video({
-         source: '../../assets/videos/cool.mp4',// './assets/cool.mp4',
+         source: '../../assets/videos/equi.mp4',// './assets/cool.mp4',
          containerId: containerSelector,
          height: window.innerHeight,
          width: window.innerWidth
@@ -69,7 +111,7 @@ export class Camera360Component implements OnInit {
   start3() {
     var containerSelector = '#container360-3';
     this.viewer3 = new Kaleidoscope.Video({
-        source: '../../assets/videos/cool.mp4',// './assets/cool.mp4',
+        source: '../../assets/videos/equi.mp4',// './assets/cool.mp4',
         containerId: containerSelector,
         height: window.innerHeight,
         width: window.innerWidth
@@ -86,7 +128,7 @@ export class Camera360Component implements OnInit {
   start4() {
     var containerSelector = '#container360-4';
     this.viewer4 = new Kaleidoscope.Video({
-        source: '../../assets/videos/cool.mp4',// './assets/cool.mp4',
+        source: '../../assets/videos/equi.mp4',// './assets/cool.mp4',
         containerId: containerSelector,
         height: window.innerHeight,
         width: window.innerWidth
